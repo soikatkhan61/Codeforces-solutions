3fb95d1df6139460958f174624e7a1fdceb00f47
Soikat Hossain <61920251+soikatkhan61@users.noreply.github.com>
2021-08-11 22:39:46 +0600

    Create 262A - Roma and Lucky Numbers.cpp

---
+++ 262A - Roma and Lucky Numbers.cpp
@@ -0,0 +1,32 @@
+//125599243	Aug/11/2021 22:38UTC+6	soikatHossain	262A - Roma and Lucky Numbers	GNU C++17	Accepted	60 ms	3700 KB
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, k, count(0);
+    cin >> n >> k;
+
+    string s;
+    while (n--)
+    {
+        int lucky(0);
+        cin >> s;
+        for (size_t i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == '4' || s[i] == '7')
+            {
+                lucky += 1;
+            }
+        }
+        if (lucky <= k)
+        {
+            count += 1;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
