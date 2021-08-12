a5fb4bee219ceb82a9f8ae6db54dfbde7d0d7e30
Soikat Hossain <61920251+soikatkhan61@users.noreply.github.com>
2021-08-12 22:22:23 +0600

    Create 291A - spyke talk.cpp

---
+++ 291A - spyke talk.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <algorithm>//fuck
+using namespace std;
+int main()
+{
+    int n, id[1000];
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> id[i];
+    }
+    sort(id, id + n);
+    int pairs = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        if (id[i] != 0 && id[i] == id[i-1])
+        {
+            pairs += 1;
+            if (i + 1 < n && id[i] == id[i+1])
+            {
+                pairs = -1;
+                break;
+            }
+        }
+    }
+    cout << pairs << endl;
+    return 0;
+
+}
